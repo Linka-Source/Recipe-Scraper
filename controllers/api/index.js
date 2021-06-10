815c90389f75ef6c9538ccfa0fad99bbb557b2bd
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-10 18:57:07 +0930

    Updated recipeAddRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const recipeAddRoutes = require('./recipeAddRoutes');
 
 router.use('/users', userRoutes);
+router.use('./recipe/add', recipeAddRoutes);
 
 module.exports = router;
