1e6b865666b04bed64643eebe8f5cf5b3c6a4c72
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-15 17:01:38 +0930

    Added pathways for seed Data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,51 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Recipe } = require('../models');
+
+const seedRecipeTitle = require('./recipeTitle');
+const seedRecipeImage = require('./recipeImage');
+const seedRecipeDescription = require('./recipeDescription');
+const seedRecipeServes = require('./recipeServes');
+const seedRecipeTime = require('./recipeTime');
+const seedRecipeIngredients = require('./RecipeIngredients');
+const seedRecipeInstructions = require('./recipeInstructions');
+const seedRecipeTags = require('./recipeTags');
+const seedRecipeURL = require('./recipeURL');
 
 const userData = require('./userData.json');
-// const projectData = require('./projectData.json');
+const importedRecipeData = require('./importedRecipeData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+
+    await seedRecipeTitle();
+    console.log('\n----- RECIPE TITLES SEEDED -----\n');
+
+    await seedRecipeImage();
+    console.log('\n----- RECIPE IMAGES SEEDED -----\n');
+
+    await seedRecipeDescription();
+    console.log('\n----- RECIPE DESCRIPTIONS SEEDED -----\n');
+
+    await seedRecipeServes();
+    console.log('\n----- RECIPE SERVINGS SEEDED -----\n');
+
+    await seedRecipeTime();
+    console.log('\n----- RECIPE COOKING TIMES SEEDED -----\n');
+
+    await seedRecipeIngredients();
+    console.log('\n----- RECIPE INGREDIENTS SEEDED -----\n');
+
+    await seedRecipeInstructions();
+    console.log('\n----- RECIPE INSTRUCTIONS SEEDED -----\n');
+
+    await seedRecipeTags();
+    console.log('\n----- RECIPE TAGS SEEDED -----\n');
+
+    await seedRecipeURL();
+    console.log('\n----- RECIPE URLS SEEDED -----\n');
+
+    const recipes = await Recipe.bulkCreate(importedRecipeData);
 
     const users = await User.bulkCreate(userData, {
         individualHooks: true,
