b125cbfe63a420cf63eb0e07a69f02666ac22709
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-16 13:39:35 +0930

    Removed saved recipescraper function

--- public/js/recipeScraper.js
+++
@@ -1,8 +0,0 @@
-// import the module
-const recipeScraper = require('recipe-scraper');
-// enter a supported recipe url as a parameter - returns a promise
-async function scrapeRecipe() {
-    let = await recipeScraper(recipeURL);
-    console.log(recipeData);
-}
-scrapeRecipe();
