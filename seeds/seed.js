a9418f521e4b01498e7e53c98319fdbaf505196b
Emma Mattner <emattner1997@gmail.com>
2021-06-17 20:03:34 +0930

    fix seed again

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { User, Recipe } = require('../models');
+const { User, Recipe, Tag, Ingredient } = require('../models');
 const RecipeSeedData = require('./RecipeSeedData');
 const userData = require('./userData.json');
 
